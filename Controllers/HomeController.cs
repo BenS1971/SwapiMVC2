b720e0d52e7c11d947ca7e1b5e33246c5a46f9f1
Ben Strycker <bgstrycker1971@gmail.com>
2024-07-09 20:32:58 -0400

    Just another commit. Nothing to see here.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace SwapiMVC2.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private static Random _random = new Random();
 
     public HomeController(ILogger<HomeController> logger)
     {
